c20a5c5e7f11861a78ba6128fddffeb334a663fa
이수민 <judy3504@naver.com>
2024-04-29 10:21:31 +0900

    [design] carousel function

--- Mobile/app/src/main/java/com/ssafy/chelitalk/common/MainActivity.java
+++ Mobile/app/src/main/java/com/ssafy/chelitalk/common/MainActivity.java
@@ -2,15 +2,21 @@ package com.ssafy.chelitalk.common;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.ssafy.chelitalk.R;
+import com.ssafy.chelitalk.carousel.MyAdapter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,20 +26,70 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
-import java.nio.Buffer;
+
+import me.relex.circleindicator.CircleIndicator3;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_URL = "https://api.adviceslip.com/advice";
     private TextView adviceTextView;
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 3;
+    private CircleIndicator3 mIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //캐러셀
+        mPager = findViewById(R.id.viewpager);
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        mPager.setCurrentItem(1000);
+        mPager.setOffscreenPageLimit(3);
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if(positionOffsetPixels == 0){
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position){
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%num_page);
+            }
+        });
+
+        final float pageMargin = getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
+
+        mPager.setPageTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float myOffset = position * -(2*pageOffset+pageMargin);
+                if(mPager.getOrientation()==ViewPager2.ORIENTATION_HORIZONTAL){
+                    if(ViewCompat.getLayoutDirection(mPager)==ViewCompat.LAYOUT_DIRECTION_RTL){
+                        page.setTranslationX(-myOffset);
+                    }else{
+                        page.setTranslationX(myOffset);
+                    }
+                }else{
+                    page.setTranslationY(myOffset);
+                }
+            }
+        });
 
         //화면에 진입하면 바로 명언띄우기
         adviceTextView  = findViewById(R.id.adviceTextView);
