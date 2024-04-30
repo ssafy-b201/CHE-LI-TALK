58144c71f480854e7ca1655389b0b4a575f95420
이예원 <2_yewon@naver.com>
2024-04-30 10:30:13 +0900

    [chore] endpoint prefix

--- Backend/src/main/java/com/ssafy/devway/domain/member/controller/MemberController.java
+++ Backend/src/main/java/com/ssafy/devway/domain/member/controller/MemberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/cherry/api/member")
+@RequestMapping("cherry/api/member")
 public class MemberController {
 
     private final MemberService memberService;
