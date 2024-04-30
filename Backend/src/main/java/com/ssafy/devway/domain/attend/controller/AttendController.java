518fec3b775e13e047f94bb90301076e0f02d9af
이수민 <judy3504@naver.com>
2024-04-30 14:31:56 +0900

    [feat] attend code update

--- Backend/src/main/java/com/ssafy/devway/domain/attend/controller/AttendController.java
+++ Backend/src/main/java/com/ssafy/devway/domain/attend/controller/AttendController.java
@@ -1,17 +1,15 @@
 package com.ssafy.devway.domain.attend.controller;
 
-import com.ssafy.devway.domain.attend.dto.request.CheckAttendRequest;
-import com.ssafy.devway.domain.attend.dto.response.WeeklyAttendResponse;
+import com.ssafy.devway.domain.attend.dto.AttendDto;
 import com.ssafy.devway.domain.attend.service.AttendService;
-import com.ssafy.devway.domain.member.entity.Member;
-import com.ssafy.devway.global.api.ApiResponse;
 import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,25 +24,25 @@ public class AttendController {
 	 * 4.1 출석 조회
 	 */
 	@GetMapping("/list/{memberId}")
-	public WeeklyAttendResponse attendList(@PathVariable Long memberId) throws IOException {
+	public List<AttendDto> attendList(@PathVariable Long memberId) throws IOException {
 		return attendService.getList(memberId);
 	}
 
 	/**
-	* 4.2 출석 업데이트해주기 : 오늘 진입하면 true로 변경
-	*/
-	@PutMapping("/update")
-	public WeeklyAttendResponse updateAttend(@RequestBody CheckAttendRequest request){
-		return attendService.updateList(request.getMember().getMemberId());
+	  * 4.2 출석 업데이트해주기
+	  */
+	@PutMapping("/update/{memberId}")
+	public ResponseEntity<String> updateAttend(@PathVariable Long memberId){
+		return ResponseEntity.ok(attendService.updateList(memberId));
 	}
 
 
 	/**
-	* 4.3 출석 초기화 (일주일 지나면)
-	*/
+	  * 4.3 출석 초기화 (일주일 지나면)
+	  */
 	@PostMapping("/reset")
-	public WeeklyAttendResponse resetAttend(@PathVariable Long memberId) throws IOException{
-		return attendService.resetList(memberId);
+	public ResponseEntity<String> resetAttend(Long memberId) throws IOException{
+		return ResponseEntity.ok(attendService.resetList(memberId));
 	}
 
 
--- Backend/src/main/java/com/ssafy/devway/domain/member/entity/Member.java
+++ Backend/src/main/java/com/ssafy/devway/domain/member/entity/Member.java
@@ -1,10 +1,15 @@
 package com.ssafy.devway.domain.member.entity;
 
+import com.ssafy.devway.domain.attend.entity.Attend;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,10 +34,14 @@ public class Member {
     @Column(name = "member_email")
     private String memberEmail;
 
+    @OneToMany(mappedBy = "member")
+    private List<Attend> memberAttend;
+
     @Builder
     public Member(String memberNickname, String memberEmail) {
         this.memberNickname = memberNickname;
         this.memberIsAlert = true;
         this.memberEmail = memberEmail;
+        this.memberAttend = new ArrayList<>();
     }
 }
