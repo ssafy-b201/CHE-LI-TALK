486ffe6434cc998bf3160268bbc18e8a19671a6d
이예원 <2_yewon@naver.com>
2024-05-02 01:05:43 +0900

    [feat] chat api 연동

--- Mobile/app/src/main/java/com/ssafy/chelitalk/activity/english/ChattingActivity.java
+++ Mobile/app/src/main/java/com/ssafy/chelitalk/activity/english/ChattingActivity.java
@@ -1,26 +1,246 @@
 package com.ssafy.chelitalk.activity.english;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ssafy.chelitalk.R;
+import com.ssafy.chelitalk.activity.common.MainActivity;
+import com.ssafy.chelitalk.activity.login.SignupActivity;
+import com.ssafy.chelitalk.api.chat.Message;
+import com.ssafy.chelitalk.api.chat.MessageAdapter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.ArrayList;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ChattingActivity extends AppCompatActivity {
+
+    private EditText etMessage;
+    private RecyclerView recyclerView;
+    private MessageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chatting);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new MessageAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Intent intent = getIntent();
+        String keyword = intent.getStringExtra("keyword");
+        String userEmail = intent.getStringExtra("userEmail");
+
+        // 선택한 키워드로 채팅 시작
+        startChat(userEmail, keyword);
+
+        // 메시지 전송
+        etMessage = findViewById(R.id.etMessage);
+        Button btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = etMessage.getText().toString();
+                if (!message.isEmpty()) {
+                    sendMessage(userEmail, message);
+                    etMessage.setText("");
+                }
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //채팅 종료
+        Button btn_finish = (Button) findViewById(R.id.btn_finish);
+        btn_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(ChattingActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.dialog_chat_finish, null);
+                dlg.setView(dialogView);
+
+                TextView tvDialogContent = dialogView.findViewById(R.id.tvDialogContent);
+                Button btnDialogOk = dialogView.findViewById(R.id.btnDialogOk);
+                Button btnDialogCancel = dialogView.findViewById(R.id.btnDialogCancel);
+
+                AlertDialog alertDialog = dlg.create();
+                alertDialog.getWindow().setBackgroundDrawableResource(R.drawable.rectangle_style);
+
+                tvDialogContent.setText("정말 채팅을 끝내시겠어요?");
+
+                btnDialogCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+                btnDialogOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(ChattingActivity.this, HistoryActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
+    }
+
+    private void startChat(String email, String keyword) {
+        try {
+
+            OkHttpClient client = getSecureOkHttpClient();
+
+            String url = "https://k10b201.p.ssafy.io/cherry/api/chat/begin";
+
+            MediaType mediaType = MediaType.parse("application/json");
+            String requestBody = "{\"memberEmail\":\"" + email + "\", \"content\":\"" + keyword + "\"}";
+            RequestBody body = RequestBody.create(requestBody, mediaType);
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            client.newCall(request).enqueue(new okhttp3.Callback() {
+
+                @Override
+                public void onFailure(okhttp3.Call call, IOException e) {
+                    Log.e("ChattingActivity", "서버 통신 실패", e);
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        // 서버로부터의 응답 처리
+                        Log.d("ChattingActivity", "메시지 전송 성공");
+                        String responseMessage = response.body().string();
+                        runOnUiThread(() -> {
+                            // 서버 응답 메시지를 RecyclerView에 추가합니다.
+                            adapter.addMessage(new Message(responseMessage, false)); // 받은 메시지로 간주
+                        });
+                    } else {
+                        // 서버 오류 처리
+                        Log.e("ChattingActivity", "서버 오류: " + response.code());
+                    }
+                }
+            });
+        }catch (Exception e) {
+            Log.e("ChattingActivity", "///// OkHttp Client Error /////", e);
+        }
+    }
+
+    private void sendMessage(String email, String message) {
+        try {
+
+            OkHttpClient client = getSecureOkHttpClient();
+
+            String url = "https://k10b201.p.ssafy.io/cherry/api/chat";
+
+            MediaType mediaType = MediaType.parse("application/json");
+            String requestBody = "{\"memberEmail\":\"" + email + "\", \"content\":\"" + message + "\"}";
+            RequestBody body = RequestBody.create(requestBody, mediaType);
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            client.newCall(request).enqueue(new okhttp3.Callback() {
+
+                @Override
+                public void onFailure(okhttp3.Call call, IOException e) {
+                    Log.e("ChattingActivity", "서버 통신 실패", e);
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        // 서버로부터의 응답 처리
+                        Log.d("ChattingActivity", "메시지 전송 성공");
+                            String responseMessage = response.body().string();
+                        runOnUiThread(() -> {
+                            adapter.addMessage(new Message(message, true));
+                            adapter.addMessage(new Message(responseMessage, false)); // 받은 메시지 추가
+                        });
+                    } else {
+                        // 서버 오류 처리
+                        Log.e("ChattingActivity", "서버 오류: " + response.code());
+                    }
+                }
+            });
+        }catch (Exception e) {
+            Log.e("ChattingActivity", "///// OkHttp Client Error /////", e);
+        }
+    }
+
+    private OkHttpClient getSecureOkHttpClient() throws Exception {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream caInput = getResources().openRawResource(R.raw.k10b201);
+        Certificate ca;
+        try {
+            ca = cf.generateCertificate(caInput);
+        } finally {
+            caInput.close();
+        }
+
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        X509TrustManager trustManager = (X509TrustManager) tmf.getTrustManagers()[0];
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new javax.net.ssl.TrustManager[]{trustManager}, null);
+
+        return new OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.getSocketFactory(), trustManager)
+                .build();
     }
 }
--- Mobile/app/src/main/java/com/ssafy/chelitalk/activity/english/SelectActivity.java
+++ Mobile/app/src/main/java/com/ssafy/chelitalk/activity/english/SelectActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -22,19 +24,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ssafy.chelitalk.R;
 import com.ssafy.chelitalk.activity.common.MainActivity;
 
 public class SelectActivity extends AppCompatActivity {
 
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser user = auth.getCurrentUser();
+
     private Spinner spinner;
-    private TextView tv_result;
+    private String userEmail = user.getEmail();
+    private String keyword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_select);
+
+        spinner = findViewById(R.id.spinner_keywords);
 
         // to speaking / to chatting
         final ImageButton btn_speak = (ImageButton) findViewById(R.id.btn_speak);
@@ -45,12 +55,8 @@ public class SelectActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                /**
-                 * todo : gpt
-                 */
                 Intent intent = new Intent(SelectActivity.this, SpeakingActivity.class);
                 startActivity(intent);
-
             }
         });
 
@@ -58,14 +64,45 @@ public class SelectActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SelectActivity.this, ChattingActivity.class);
-                startActivity(intent);
+                if (keyword != null && !keyword.isEmpty()) {
+                    Intent intent = new Intent(SelectActivity.this, ChattingActivity.class);
+                    intent.putExtra("keyword", keyword);
+                    intent.putExtra("userEmail", userEmail);
+                    startActivity(intent);
+                } else {
+                    // 키워드가 설정되지 않은 경우 오류 메시지를 표시하거나 처리
+                    Toast.makeText(SelectActivity.this, "키워드를 선택하세요.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_selected_layout, getResources().getStringArray(R.array.select_keywords)) {
+            @NonNull
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = convertView;
+                if (view == null) {
+                    view = getLayoutInflater().inflate(R.layout.spinner_dropdown_layout, parent, false);
+                }
+                TextView textView = view.findViewById(R.id.text_view_spinner_item);
+                textView.setText(getItem(position));
+                return view;
+            }
+        };
 
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                keyword = (String) parent.getItemAtPosition(position);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+
+        });
 
         //메뉴 dialog(modal)
         final ImageButton button1 = (ImageButton) findViewById(R.id.imageButton);
@@ -123,43 +160,6 @@ public class SelectActivity extends AppCompatActivity {
                 dlg.show();
             }
         });
-
-
-        spinner = findViewById(R.id.spinner_keywords);
-
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_selected_layout, getResources().getStringArray(R.array.select_keywords)) {
-            @NonNull
-            @Override
-            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                View view = convertView;
-                if (view == null) {
-                    view = getLayoutInflater().inflate(R.layout.spinner_dropdown_layout, parent, false);
-                }
-                TextView textView = view.findViewById(R.id.text_view_spinner_item);
-                textView.setText(getItem(position));
-                return view;
-            }
-        };
-
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                /**
-                 * todo : gpt
-                 */
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-
-        });
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
