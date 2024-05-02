0ac6d521fb17c05303688d55e9bca369677c0bbb
이예원 <2_yewon@naver.com>
2024-05-02 17:35:57 +0900

    [design] 메인 모달 버튼 Activity 추가

--- Mobile/app/src/main/java/com/ssafy/chelitalk/activity/english/HistoryActivity.java
+++ Mobile/app/src/main/java/com/ssafy/chelitalk/activity/english/HistoryActivity.java
@@ -1,14 +1,26 @@
 package com.ssafy.chelitalk.activity.english;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.ssafy.chelitalk.R;
+import com.ssafy.chelitalk.activity.common.MainActivity;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -21,6 +33,75 @@ public class HistoryActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //메뉴 dialog(modal)
+        final ImageButton button1 = (ImageButton) findViewById(R.id.imageButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(HistoryActivity.this);
+                dlg.setTitle("Menu");
+                ListAdapter adapter = new ArrayAdapter<String>(
+                        HistoryActivity.this, R.layout.dialog_item, R.id.text, new String[]{"HOME", "STUDY", "LIKE", "HISTORY", "CHECK"}) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
+                        ImageView img = view.findViewById(R.id.icon);
+                        switch (position) {
+                            case 0:
+                                img.setImageResource(R.drawable.home_icon);
+                                break;
+                            case 1:
+                                img.setImageResource(R.drawable.study_icon);
+                                break;
+                            case 2:
+                                img.setImageResource(R.drawable.like_icon);
+                                break;
+                            case 3:
+                                img.setImageResource(R.drawable.history_icon);
+                                break;
+                            case 4:
+                                img.setImageResource(R.drawable.check_icon);
+                                break;
+                            default:
+                                img.setImageResource(R.drawable.cherry);
+                                break;
+                        }
+                        return view;
+                    }
+                };
+
+                dlg.setAdapter(adapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent;
+                        switch (which) {
+                            case 0:
+                                intent = new Intent(HistoryActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 1:
+                                intent = new Intent(HistoryActivity.this, SelectActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 2:
+                                intent = new Intent(HistoryActivity.this, LikeActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 3:
+                                intent = new Intent(HistoryActivity.this, HistoryActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 4:
+                                intent = new Intent(HistoryActivity.this, CheckActivity.class);
+                                startActivity(intent);
+                                break;
+                        }
+                    }
+                });
+                dlg.show();
+            }
         });
     }
 }
