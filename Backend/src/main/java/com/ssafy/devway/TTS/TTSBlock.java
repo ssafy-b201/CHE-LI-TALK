0036e2db95ff4aa0f4718bc720316c7732648d70
이예원 <2_yewon@naver.com>
2024-05-07 12:29:28 +0900

    [chore] docker-compose 수정

--- Backend/src/main/java/com/ssafy/devway/TTS/TTSBlock.java
+++ Backend/src/main/java/com/ssafy/devway/TTS/TTSBlock.java
@@ -1,11 +1,6 @@
 package com.ssafy.devway.TTS;
 
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.texttospeech.v1.SsmlVoiceGender;
 import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
-import com.google.cloud.texttospeech.v1.TextToSpeechSettings;
-import com.google.common.collect.Lists;
 import com.ssafy.devway.block.element.BlockElement;
 import com.google.cloud.texttospeech.v1.AudioConfig;
 import com.google.cloud.texttospeech.v1.SynthesisInput;
@@ -13,9 +8,6 @@ import com.google.cloud.texttospeech.v1.TextToSpeechClient;
 import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
 import com.google.cloud.texttospeech.v1.AudioEncoding;
 import com.google.protobuf.ByteString;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import lombok.Data;
 
 
@@ -38,7 +30,6 @@ public class TTSBlock implements BlockElement {
 
         model = country.getTextMode();
 
-//        try (TextToSpeechClient textToSpeechClient = initializeTextToSpeechClient()) {
         try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()){
             SynthesisInput input = SynthesisInput.newBuilder()
                 .setText(text)
@@ -62,20 +53,4 @@ public class TTSBlock implements BlockElement {
         }
     }
 
-    public TextToSpeechClient initializeTextToSpeechClient() throws Exception {
-        // 인증 파일 경로 지정
-        String jsonPath = "/home/ubuntu/MyGC.json";
-
-        // 파일에서 인증 정보 로드
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath))
-            .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-
-        // 클라이언트 설정에 인증 정보 적용
-        TextToSpeechSettings textToSpeechSettings = TextToSpeechSettings.newBuilder()
-            .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
-            .build();
-
-        // 설정을 사용하여 TextToSpeechClient 생성
-        return TextToSpeechClient.create(textToSpeechSettings);
-    }
 }
